bece0f8878de677505ee51277464760f5ffd8e30
Donique South <donique.south@ucalgary.ca>
2025-03-10 14:08:33 -0600

    added loop, score system, and tracker for attempts.
    could maybe clean up the code, maybe use functions???
    
    anyways, it's functional.

--- rock_p_s.py
+++ rock_p_s.py
@@ -2,44 +2,57 @@ import random
 
 opts = ["rock", "paper", "scissors"]
 
-to_guess = random.choice(opts)
-l_to_guess = to_guess.lower()
+print("Hello! Welcome to rock, paper, scissors!")
 
-guess = input("Choose rock, paper, or scissors: ")
-l_guess = guess.lower()
 
+
+score = 0
 counter = 0
 
+while counter < 6:
+    guess = input("Choose rock, paper, or scissors: ")
+    l_guess = guess.lower()
 
-while(counter <= 6):
+    to_guess = random.choice(opts)
+    l_to_guess = to_guess.lower()
 
-    if(l_to_guess=="rock"):
-        elif(l_guess=="rock"):
-            print("You lost!")
-        elif (l_guess == "scissors"):
-            print("You lost!")
-        elif (l_guess == "paper"):
-            print("You won!")
-
-    if(l_to_guess=="paper"):
-        if(l_guess=="paper"):
-            print("You lost!")
-        if (l_guess == "rock"):
-            print("You lost!")
-        if (l_guess == "scissors"):
-            print("You won!")
-
-
-    if(l_to_guess=="scissors"):
-        if(l_guess=="paper"):
-            print("You lost!")
-        if (l_guess == "scissors"):
-            print("You lost!")
-        if (l_guess == "rock"):
-            print("You won!")
-
-    if(l_to_guess not in opts):
+    if l_guess not in opts:
         print("Please type rock, paper, or scissors.")
+        continue
+
+    if l_guess == "help":
+        print("You have 6 rounds to beat the game!\nRemember, rock beats scissors, paper beats rock, and scissors beats paper.")
+
+    if l_to_guess == "rock":
+        if l_guess == "rock":
+            print("You lost!")
+        if l_guess == "scissors":
+            print("You lost!")
+        if l_guess == "paper":
+            score+=1
+            print("You won!")
+
+    if l_to_guess=="paper":
+        if l_guess=="paper":
+            print("You lost!")
+        if l_guess == "rock":
+            print("You lost!")
+        if l_guess == "scissors":
+            print("You won!")
+            score += 1
+
+    if l_to_guess=="scissors":
+        if l_guess=="paper":
+            print("You lost!")
+        if l_guess == "scissors":
+            print("You lost!")
+        if l_guess == "rock":
+            print("You won!")
+            score += 1
 
     counter += 1
+    print("\nYour remaining attempts: " + str(6 - counter)+"\n")
+
+printsc = score * 100
+print("Your final score is: " + str(printsc) + "\nSee you next time!")
 
