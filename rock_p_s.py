d24fa033bbd42d8d1e7ce35eee531328947e496b
Donique South <donique.south@ucalgary.ca>
2025-03-10 11:34:54 -0600

    the base game is functional, want to clean it up though. (Could make functions and add a loop as well as add a score)

--- rock_p_s.py
+++ rock_p_s.py
@@ -3,8 +3,38 @@ import random
 opts = ["rock", "paper", "scissors"]
 
 to_guess = random.choice(opts)
+print(to_guess)
 
-guess = input("Choose rock, paper, or scissors.")
+guess = input("Choose rock, paper, or scissors: ")
 
-if(to_guess == guess):
-    print("You lost!")
+#if(to_guess == guess):
+  #  print("You lost!")
+
+if(to_guess=="rock"):
+    if(guess=="rock"):
+        print("You lost!")
+    if (guess == "scissors"):
+        print("You lost!")
+    if (guess == "paper"):
+        print("You won!")
+
+if(to_guess=="paper"):
+    if(guess=="paper"):
+        print("You lost!")
+    if (guess == "rock"):
+        print("You lost!")
+    if (guess == "scissors"):
+        print("You won!")
+
+
+if(to_guess=="scissors"):
+    if(guess=="paper"):
+        print("You lost!")
+    if (guess == "scissors"):
+        print("You lost!")
+    if (guess == "rock"):
+        print("You won!")
+
+if(guess not in opts):
+    print("Please type rock, paper, or scissors.")
+
